0c3be0c5e16fac475d8056f6d41343c1f44a9e3f
unknown <ahmedmahrous015@gmail.com>
2023-03-10 11:57:49 +0200

    first commit

--- birthday_card_app/lib/main.dart
+++ birthday_card_app/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BirthdayCard());
+  runApp(const BirthdayCard());
 }
 
 class BirthdayCard extends StatelessWidget {
+  const BirthdayCard({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xFFD2BCD5),
